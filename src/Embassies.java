e923fa4c8721ac37ff6ee27fac948e6b5abfb4cb
quentindamois <quentin.damois@efrei.net>
2023-10-15 21:04:27 -0400

    creation of the Embassies class

---
+++ src/Embassies.java
@@ -0,0 +1,12 @@
+public class Embassies {
+    private String country;
+    public boolean receiveVisa( String traveler) {
+        return true;
+    }
+    public boolean receivePAssePortCode(int passePortCode) {
+        return true;
+    }
+    public String getCountry() {
+        return this.country;
+    }
+}
