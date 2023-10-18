0689e08a854e5ba83081ce7662ef38e9d6b1c011
Alexis GEORGES <83827885+monkeypot@users.noreply.github.com>
2023-10-18 16:23:28 -0400

    getter

--- src/BuyTicketPanel.java
+++ src/BuyTicketPanel.java
@@ -6,7 +6,8 @@ import java.util.ArrayList;
 public class BuyTicketPanel extends JPanel {
     
     private ArrayList<JButton> buttons = new ArrayList<JButton>();
-
+    private ArrayList<String> price = new ArrayList<>();
+    private ArrayList<String> date = new ArrayList<>();
     public BuyTicketPanel(JFrame frame, JPanel lastPanel, String[][] listTickets){
 
         // Create a vertical box layout to hold the ticket information and buttons
@@ -16,6 +17,8 @@ public class BuyTicketPanel extends JPanel {
         // Create a list of buttons to display the ticket information
         for (int i = 0; i < listTickets.length; i++) {
             String[] ticket = listTickets[i];
+            price.add(ticket[0]);
+            date.add(ticket[1]);
             JButton button = new JButton(ticket[0] + " - " + ticket[1]);
             button.addActionListener(new ActionListener() {
                 public void actionPerformed(ActionEvent e) {
@@ -34,6 +37,12 @@ public class BuyTicketPanel extends JPanel {
     }
     public ArrayList<JButton> getButtons() {
         return buttons;
+    }
+    public ArrayList<String> getDate() {
+        return date;
+    }
+    public ArrayList<String> getPrice() {
+        return price;
     }
 }
             
